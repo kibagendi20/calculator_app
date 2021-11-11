ef7bb9978c6008588bb604a51702c8242dea370d
Kibagendi Mainga <maingawesley@gmail.com>
2021-11-12 02:30:01 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:calculator_app/widgets/bill.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(MaterialApp(home: BillSplitter()));
+}
---
+++ lib/widgets/bill.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BillSplitter extends StatefulWidget {
+  const BillSplitter({Key? key}) : super(key: key);
+
+  @override
+  _BillSplitterState createState() => _BillSplitterState();
+}
+
+class _BillSplitterState extends State<BillSplitter> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
