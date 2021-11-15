d003e971f4c414cff8f82cda921d27db4db3c1a2
Kibagendi Mainga <maingawesley@gmail.com>
2021-11-15 07:53:13 +0300

    Initial Commit

--- lib/widgets/bill.dart
+++ lib/widgets/bill.dart
@@ -8,8 +8,83 @@ class BillSplitter extends StatefulWidget {
 }
 
 class _BillSplitterState extends State<BillSplitter> {
+  int _tipPercentage = 0;
+  int _personCounter = 1;
+  double _billAmount = 0.0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
+        alignment: Alignment.center,
+        color: Colors.white,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          padding: EdgeInsets.all(20.5),
+          children: [
+            Container(
+              height: 150,
+              width: 150,
+              decoration: BoxDecoration(
+                color: Colors.purpleAccent.shade100,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Total Per Person"),
+                    Text("123"),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+              padding: EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.all(
+                    color: Colors.blueAccent.shade100,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.circular(12.0)),
+              child: Column(
+                children: [
+                  TextField(
+                    keyboardType:
+                        TextInputType.numberWithOptions(decimal: true),
+                    style: TextStyle(color: Colors.grey),
+                    decoration: InputDecoration(
+                      prefixText: "Bill Amount",
+                      prefixIcon: Icon(Icons.attach_money),
+                    ),
+                    onChanged: (String value) {
+                      try {
+                        _billAmount = double.parse(value);
+                      } catch (exception) {
+                        _billAmount = 0.0;
+                      }
+                    },
+                  ),
+                  // SizedBox(
+                  //   height: 20,
+                  // ),
+                  // TextField(
+                  //   decoration: InputDecoration(
+                  //     border: OutlineInputBorder(),
+                  //     hintText: "Enter a search term",
+                  //     label: Text("email"),
+                  //     errorText: "Incorrect",
+                  //     prefixIcon: Icon(Icons.access_alarm),
+                  //   ),
+                  // ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
